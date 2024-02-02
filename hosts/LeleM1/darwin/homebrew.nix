4ae577e15740f09deab9aa7e691f953e50956ae0
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-02-02 02:33:57 +0100

    LeleM1: add few Brew apps and uninstalled apps not managed via nix-darwin

--- hosts/LeleM1/darwin/homebrew.nix
+++ hosts/LeleM1/darwin/homebrew.nix
@@ -3,19 +3,70 @@
   homebrew = {
     enable = true;
 
-    onActivation.upgrade = true;
+    global.autoUpdate = true;
+    onActivation = {
+      upgrade = true;
+      cleanup = "uninstall";
+      extraFlags = [
+        "--force"
+      ];
+    };
 
-    # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
+    # Taps
+    taps = [
+      "osx-cross/avr"
+      "osx-cross/arm"
+    ];
+
+    # Apps from the Apple Store
+    #masApps = [];
+
+    # Brews
+    brews = [
+      "colima" # based on lima - Docker in Lima VM
+      "lima" # QEMU GUI tool
+      "teensy_loader_cli" # needed for some ESP32 or ESP8266
+    ];
+
+    # Casks
     casks = [
       "alfred"
       "amethyst"
+      "autodesk-fusion360"
+      "balenaetcher"
+      "bartender"
+      "bitwarden"
+      "cryptomator"
       "dbeaver-community"
       "discord"
+      "displaylink"
+      "elgato-stream-deck"
+      "firefox"
+      "gimp"
       "hammerspoon"
       "iina"
-      "logseq"
-      "rectangle"
-      "warp"
+      "inkscape"
+      "istat-menus"
+      "keybase"
+      "obs"
+      "obs-virtualcam"
+      "obsbot-center"
+      "obsidian" # notetaking 2nd brain
+      "orbstack" # https://github.com/OrbStack/orbstack - docker on steroid
+      "pock" # macOS dock app (made by an italian)
+      "qmk-toolbox"
+      "rectangle" # tiling window manager for Mac
+      "smplayer"
+      "spotify"
+      "syncthing"
+      "tailscale"
+      "telegram-desktop"
+      "utm" # QEMU GUI
+      "vivaldi" # QEMU GUI
+      "vlc"
+      "vlc-webplugin"
+      "warp" # https://warp.dev - fancy terminal
+      "zoom"
     ];
   };
 }
--- hosts/LeleM1/home-manager/programs.nix
+++ hosts/LeleM1/home-manager/programs.nix
@@ -14,6 +14,7 @@
     ./programs/neovim.nix
     ./programs/zellij.nix
     ./programs/zsh.nix
+    ./programs/alacritty.nix
   ];
 
 } # EOF
