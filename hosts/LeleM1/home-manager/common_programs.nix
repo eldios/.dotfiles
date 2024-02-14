6559f3c3c89088116d5f9fae4dd9ae4d933fecc8
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-02-14 02:01:45 +0100

    LeleM1: fix mac home directory

--- hosts/LeleM1/home-manager/common_programs.nix
+++ hosts/LeleM1/home-manager/common_programs.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 let
-  repo_dir            = "${config.home.homeDirectory}/.dotfiles";
+  #repo_dir            = "${config.home.homeDirectory}/.dotfiles";
+  repo_dir            = "/Users/eldios/.dotfiles";
   common_mods_dir     = "${repo_dir}/common";
   common_hm_dir       = "${common_mods_dir}/home-manager/eldios";
   common_programs_dir = "${common_hm_dir}/programs";
