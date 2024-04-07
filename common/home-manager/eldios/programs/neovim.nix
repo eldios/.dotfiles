1eed7a8d96eefd84006d54ece062892da75cb25f
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-04-07 02:49:47 +0200

    neovim: add codeium and obsidian to neovim

--- common/home-manager/eldios/programs/neovim.nix
+++ common/home-manager/eldios/programs/neovim.nix
@@ -17,6 +17,8 @@
       ghc
       # vars
       ripgrep     # used by space-f-g
+      ripgrep-all # used by space-f-g
+      codeium
     ];
 
   };
@@ -93,6 +95,33 @@
   '';
   xdg.configFile."nvim/lua/config/autocmds.lua".text = ''
   '';
+  xdg.configFile."nvim/lua/plugins/obsidian.lua".text = ''
+    return {
+      "epwalsh/obsidian.nvim",
+      version = "*",  -- recommended, use latest release instead of latest commit
+      lazy = true,
+      ft = "markdown",
+      -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+      -- event = {
+      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      --   "BufReadPre path/to/my-vault/**.md",
+      --   "BufNewFile path/to/my-vault/**.md",
+      -- },
+      dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+      },
+      opts = {
+        workspaces = {
+          {
+            name = "2nd Brain",
+            path = "~/2nd Brain",
+          },
+        },
+      },
+    }
+  '';
   xdg.configFile."nvim/lua/plugins/disabled.lua".text = ''
     return {
       -- disable plugins
@@ -115,10 +144,11 @@
       withPython3 = true;
 
       extraPackages = with pkgs; [
-        codeium
       ];
 
       plugins = with pkgs.vimPlugins; [
+        codeium-vim
+        nvim-cmp
       ];
 
       extraConfig = ''
