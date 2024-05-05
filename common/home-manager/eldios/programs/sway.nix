bd8536e4d42bac0fd74e38e817051b1560f6e41a
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-06 00:55:00 +0200

    sway: add shortcut for mailspring

--- common/home-manager/eldios/programs/sway.nix
+++ common/home-manager/eldios/programs/sway.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
   quick_menu = "rofi -show run -show-icons -fixed-num-lines -sorting-method fzf -drun-show-actions -sidebar-mode -steal-focus -window-thumbnail";
@@ -100,7 +100,7 @@ in
       glpaper
       gnome.adwaita-icon-theme
       gnome.gnome-themes-extra
-      gnome3.adwaita-icon-theme  # default gnome cursors
+      gnome3.adwaita-icon-theme # default gnome cursors
       grim # screenshot functionality
       grimblast # screenshot functionality
       gsettings-desktop-schemas
@@ -185,7 +185,7 @@ in
       right = "l";
 
       # Set default terminal
-      terminal = "wezterm"; 
+      terminal = "wezterm";
       menu = "rofi";
 
       startup = [
@@ -199,7 +199,7 @@ in
         "Virtual-1" = {
           res = "1920x1080";
           mode = "1920x1080@60Hz";
-          pos = "0,0"; 
+          pos = "0,0";
           transform = "0";
         };
         # "*" = { # change background for all outputs
@@ -210,86 +210,87 @@ in
       workspaceLayout = "default";
 
       keybindings = {
-        "${modifier}+Return"          = "exec ${terminal}";
+        "${modifier}+Return" = "exec ${terminal}";
+        "${modifier}+Shift+m" = "exec mailspring --password-store=\"gnome-libsecret\"";
 
-        "${modifier}+d"               = "exec ${full_menu}";
-        "${modifier}+Shift+d"         = "exec ${quick_menu}";
+        "${modifier}+d" = "exec ${full_menu}";
+        "${modifier}+Shift+d" = "exec ${quick_menu}";
 
-        "${modifier}+Shift+c"         = "kill";
-        "${modifier}+f"               = "fullscreen toggle";
+        "${modifier}+Shift+c" = "kill";
+        "${modifier}+Ctrl+f" = "focus mode_toggle";
+        "${modifier}+f" = "fullscreen toggle";
 
-        "${modifier}+Ctrl+q"          = "exec ${lockscreen}";
+        "${modifier}+Ctrl+q" = "exec ${lockscreen}";
 
-        "${modifier}+Shift+Ctrl+q"    = "exec ${powermenu}";
+        "${modifier}+Shift+Ctrl+q" = "exec ${powermenu}";
 
-        "${modifier}+Shift+Ctrl+r"    = "reload";
-        "${modifier}+Ctrl+r"          = "mode resize";
+        "${modifier}+Shift+Ctrl+r" = "reload";
+        "${modifier}+Ctrl+r" = "mode resize";
 
-        "${modifier}+Shift+Space"     = "floating toggle";
-        "${modifier}+Space"           = "focus mode_toggle";
-        "${modifier}+u"               = "focus parent";
+        "${modifier}+Shift+Space" = "floating toggle";
+        "${modifier}+u" = "focus parent";
 
-        "${modifier}+s"               = "layout stacking";
-        "${modifier}+w"               = "layout tabbed";
-        "${modifier}+e"               = "layout default";
+        "${modifier}+s" = "layout stacking";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+e" = "layout default";
 
-        "${modifier}+v"               = "layout toggle split";
-        "${modifier}+Shift+v"         = "splith";
-        "${modifier}+Ctrl+v"          = "splitv";
+        "${modifier}+v" = "layout toggle split";
+        "${modifier}+Shift+v" = "splith";
+        "${modifier}+Ctrl+v" = "splitv";
 
-        "${modifier}+${left}"         = "focus left";
-        "${modifier}+${down}"         = "focus down";
-        "${modifier}+${up}"           = "focus up";
-        "${modifier}+${right}"        = "focus right";
+        "${modifier}+${left}" = "focus left";
+        "${modifier}+${down}" = "focus down";
+        "${modifier}+${up}" = "focus up";
+        "${modifier}+${right}" = "focus right";
 
-        "${modifier}+Shift+${left}"   = "move left";
-        "${modifier}+Shift+${down}"   = "move down";
-        "${modifier}+Shift+${up}"     = "move up";
-        "${modifier}+Shift+${right}"  = "move right";
+        "${modifier}+Shift+${left}" = "move left";
+        "${modifier}+Shift+${down}" = "move down";
+        "${modifier}+Shift+${up}" = "move up";
+        "${modifier}+Shift+${right}" = "move right";
 
-        "${modifier}+Ctrl+${left}"    = "workspace prev";
-        "${modifier}+Ctrl+${right}"   = "workspace next";
+        "${modifier}+Ctrl+${left}" = "workspace prev";
+        "${modifier}+Ctrl+${right}" = "workspace next";
 
-        "${modifier}+minus"           = "scratchpad show";
-        "${modifier}+Shift+minus"     = "move scratchpad";
+        "${modifier}+minus" = "scratchpad show";
+        "${modifier}+Shift+minus" = "move scratchpad";
 
-        "${modifier}+Alt+${left}"     = "move workspace to output left";
-        "${modifier}+Alt+${down}"     = "move workspace to output down";
-        "${modifier}+Alt+${up}"       = "move workspace to output up";
-        "${modifier}+Alt+${right}"    = "move workspace to output right";
+        "${modifier}+Alt+${left}" = "move workspace to output left";
+        "${modifier}+Alt+${down}" = "move workspace to output down";
+        "${modifier}+Alt+${up}" = "move workspace to output up";
+        "${modifier}+Alt+${right}" = "move workspace to output right";
 
-        "${modifier}+1"               = "workspace number 1";
-        "${modifier}+2"               = "workspace number 2";
-        "${modifier}+3"               = "workspace number 3";
-        "${modifier}+4"               = "workspace number 4";
-        "${modifier}+5"               = "workspace number 5";
-        "${modifier}+6"               = "workspace number 6";
-        "${modifier}+7"               = "workspace number 7";
-        "${modifier}+8"               = "workspace number 8";
-        "${modifier}+9"               = "workspace number 9";
-        "${modifier}+0"               = "workspace number 10";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
 
-        "${modifier}+Shift+1"         = "move container to workspace number 1";
-        "${modifier}+Shift+2"         = "move container to workspace number 2";
-        "${modifier}+Shift+3"         = "move container to workspace number 3";
-        "${modifier}+Shift+4"         = "move container to workspace number 4";
-        "${modifier}+Shift+5"         = "move container to workspace number 5";
-        "${modifier}+Shift+6"         = "move container to workspace number 6";
-        "${modifier}+Shift+7"         = "move container to workspace number 7";
-        "${modifier}+Shift+8"         = "move container to workspace number 8";
-        "${modifier}+Shift+9"         = "move container to workspace number 9";
-        "${modifier}+Shift+0"         = "move container to workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
 
-        "XF86AudioRaiseVolume"  = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-        "XF86AudioLowerVolume"  = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-        "XF86AudioMute"         = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-        "XF86AudioMicMute"      = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+        "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
         "XF86MonBrightnessDown" = "exec sudo light -U 5%";
-        "XF86MonBrightnessUp"   = "exec sudo light -A 5%";
-        "Shift+XF86AudioPlay"   = "exec playerctl play-pause";
-        "XF86AudioPlay"         = "exec playerctl -p spotify play-pause";
-        "XF86AudioNext"         = "exec playerctl -p spotify next";
-        "XF86AudioPrev"         = "exec playerctl -p spotify previous";
+        "XF86MonBrightnessUp" = "exec sudo light -A 5%";
+        "Shift+XF86AudioPlay" = "exec playerctl play-pause";
+        "XF86AudioPlay" = "exec playerctl -p spotify play-pause";
+        "XF86AudioNext" = "exec playerctl -p spotify next";
+        "XF86AudioPrev" = "exec playerctl -p spotify previous";
       }; # EOM keybindings
 
     }; # EOM config
