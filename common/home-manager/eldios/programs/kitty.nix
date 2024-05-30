4f7feeedb4b693e900b2eeaebb391a02196cdc44
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-30 21:27:39 +0200

    kitty: fix kitty configuration

--- common/home-manager/eldios/programs/kitty.nix
+++ common/home-manager/eldios/programs/kitty.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   programs = {
 
@@ -7,10 +8,9 @@
         font_size = "12.0";
         background_opacity = "0.9";
         dynamic_background_opacity = "yes";
+        shell = "${pkgs.zsh}/bin/zsh -l";
       };
-    };
-
+    }; # EOM kitty
   }; # EOM programs
-
 } # EOF
 # vim: set ts=2 sw=2 et ai list nu
