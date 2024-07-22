bec3052e7b6d0dcf3bb2bab866f0e937eb5aa9cb
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-07-22 17:32:39 +0200

    lele9iyoga: add uqm and pcloud

--- hosts/lele9iyoga/home-manager/pkgs.nix
+++ hosts/lele9iyoga/home-manager/pkgs.nix
@@ -7,21 +7,34 @@ let
       ];
     }));
   });
+  patchelfFixes = pkgs.patchelfUnstable.overrideAttrs (_finalAttrs: _previousAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "Patryk27";
+      repo = "patchelf";
+      rev = "527926dd9d7f1468aa12f56afe6dcc976941fedb";
+      sha256 = "sha256-3I089F2kgGMidR4hntxz5CKzZh5xoiUwUsUwLFUEXqE=";
+    };
+  });
+  pcloud = pkgs.pcloud.overrideAttrs (_finalAttrs:previousAttrs: {
+    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ patchelfFixes ];
+  });
 in
 {
   home = {
     packages = with pkgs; [
-      dbeaver
       davinci-resolve
+      dbeaver
       guvcview
       quickemu
       quickgui
-      spice
       remmina
+      spice
+      uqm
       uvcdynctrl
       vlc
     ] ++ [
       davinci-resolve-studio
+      pcloud
     ];
   };
 } # EOF
