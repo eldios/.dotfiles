017202bf4cc499625455b83bbf1f7961a73e8f42
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-09-20 15:34:05 +0200

    lele9iyoga: fix resolve-studio

--- hosts/lele9iyoga/home-manager/pkgs.nix
+++ hosts/lele9iyoga/home-manager/pkgs.nix
@@ -1,20 +1,21 @@
 { inputs, pkgs, nixpkgs-unstable, ... }:
 let
+  system = "x86_64-linux";
   unstablePkgs = import nixpkgs-unstable {
     system = "x86_64-linux";
     config.allowUnfree = true;
   };
-  davinci-resolve-studio = pkgs.davinci-resolve-studio.override (old: {
-    buildFHSEnv = a: (old.buildFHSEnv (a // {
-      extraBwrapArgs = a.extraBwrapArgs ++ [
-        "--bind /run/opengl-driver/etc/OpenCL /etc/OpenCL"
-      ];
-    }));
-  });
+  #davinci-resolve-studio = unstablePkgs.davinci-resolve-studio.override (old: {
+  #  buildFHSEnv = a: (old.buildFHSEnv (a // {
+  #    extraBwrapArgs = a.extraBwrapArgs ++ [
+  #      "--bind /run/opengl-driver/etc/OpenCL /etc/OpenCL"
+  #    ];
+  #  }));
+  #});
 in
 {
   home = {
-    packages = with pkgs; [
+    packages = (with pkgs; [
       barrier
       davinci-resolve
       dbeaver-bin
@@ -27,9 +28,10 @@ in
       uvcdynctrl
       vlc
       wine
-    ] ++ [
+    ]) ++ (with unstablePkgs; [
       davinci-resolve-studio
+    ]) ++ ([
       inputs.zen-browser.packages."${system}".specific
-    ] ++ (with unstablePkgs; [ ]);
+    ]);
   };
 } # EOF
