7f6f5a5862e97b45468ca4467e0507969dfe8e85
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-12-03 18:24:56 +0100

    lele8845ace: fix AMDGPU driver

--- hosts/lele8845ace/nixos/system.nix
+++ hosts/lele8845ace/nixos/system.nix
@@ -174,12 +174,18 @@ in
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        amdvlk
         rocmPackages.clr.icd
       ] ;
-      extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
-      ] ;
+    };
+
+    amdgpu = {
+      amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+        supportExperimental.enable = true;
+      };
+      opencl.enable = true;
+      initrd.enable = true;
     };
 
     keyboard.qmk.enable = true;
