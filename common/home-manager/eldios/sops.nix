f6ee3e40386f29227f20160d1b7a7a954b3cf881
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-04-19 22:57:40 +0200

    anytype: add config

--- common/home-manager/eldios/sops.nix
+++ common/home-manager/eldios/sops.nix
@@ -31,6 +31,15 @@ in
       "tokens/litellm/aider/key" = { };
       "tokens/litellm/aider/url" = { };
       "tokens/kagi/key" = { };
+      "keys/anytype/id" = { };
+      "keys/anytype/networkId" = { };
+      "keys/anytype/host" = { };
+      "keys/anytype/peerId/node-1" = { };
+      "keys/anytype/peerId/node-2" = { };
+      "keys/anytype/peerId/node-3" = { };
+      "keys/anytype/peerId/coordinator" = { };
+      "keys/anytype/peerId/filenode" = { };
+      "keys/anytype/peerId/consensusnode" = { };
     };
 
     templates = {
@@ -42,6 +51,62 @@ in
           openai-api-base: "${config.sops.placeholder."tokens/litellm/aider/url"}"
         '';
         path = "/home/eldios/.aider.conf.yml";
+      };
+      "anytype.conf.yml" = {
+        content = ''
+          id: ${config.sops.placeholder."keys/anytype/id"}
+          networkId: ${config.sops.placeholder."keys/anytype/networkId"}
+          nodes:
+            - addresses:
+                - any-sync-node-1:1001
+                - quic://any-sync-node-1:1011
+                - ${config.sops.placeholder."keys/anytype/host"}:1001
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1011
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/node-1"}
+              types:
+                - tree
+            - addresses:
+                - any-sync-node-2:1002
+                - quic://any-sync-node-2:1012
+                - ${config.sops.placeholder."keys/anytype/host"}:1002
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1012
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/node-2"}
+              types:
+                - tree
+            - addresses:
+                - any-sync-node-3:1003
+                - quic://any-sync-node-3:1013
+                - ${config.sops.placeholder."keys/anytype/host"}:1003
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1013
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/node-3"}
+              types:
+                - tree
+            - addresses:
+                - any-sync-coordinator:1004
+                - quic://any-sync-coordinator:1014
+                - ${config.sops.placeholder."keys/anytype/host"}:1004
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1014
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/coordinator"}
+              types:
+                - coordinator
+            - addresses:
+                - any-sync-filenode:1005
+                - quic://any-sync-filenode:1015
+                - ${config.sops.placeholder."keys/anytype/host"}:1005
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1015
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/filenode"}
+              types:
+                - file
+            - addresses:
+                - any-sync-consensusnode:1006
+                - quic://any-sync-consensusnode:1016
+                - ${config.sops.placeholder."keys/anytype/host"}:1006
+                - quic://${config.sops.placeholder."keys/anytype/host"}:1016
+              peerId: ${config.sops.placeholder."keys/anytype/peerId/consensusnode"}
+              types:
+                - consensus
+        '';
+        path = "/home/eldios/.config/anytype.conf.yml";
       };
     };
 
