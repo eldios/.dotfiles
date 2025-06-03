6014884a269ac719da732971b05268b383820d31
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-06-03 20:21:37 +0200

    kube-casa1 and lele8835ace: update system state to 25.05

--- hosts/kube-casa1/nixos/system.nix
+++ hosts/kube-casa1/nixos/system.nix
@@ -1,7 +1,7 @@
 { peerix, pkgs, ... }:
 {
   system = {
-    stateVersion = "24.11"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "25.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     autoUpgrade.enable = true;
   };
 
--- hosts/lele8845ace/nixos/system.nix
+++ hosts/lele8845ace/nixos/system.nix
@@ -7,7 +7,7 @@ let
 in
 {
   system = {
-    stateVersion = "24.11"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "25.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     autoUpgrade.enable = true;
   };
 
