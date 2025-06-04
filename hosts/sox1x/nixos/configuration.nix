8957fdef65b3f49a8643b939b3fff7668de0e7de
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-06-04 19:15:23 +0200

    sox1x: add backupFileExtension

--- hosts/sox1x/nixos/configuration.nix
+++ hosts/sox1x/nixos/configuration.nix
@@ -30,6 +30,8 @@
 
       home-manager.nixosModules.home-manager
       {
+        home-manager.backupFileExtension = "hm-backup";
+
         home-manager.users.eldios = import ../home-manager/home.nix;
 
         home-manager.sharedModules = [
