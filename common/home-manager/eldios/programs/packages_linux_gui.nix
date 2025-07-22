5f6f90bae8fc425721fab8191b1ef94c8bd5d031
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-07-22 03:58:41 +0200

    pkgs GUI: linting + disable streamcontroller (broken)

--- common/home-manager/eldios/programs/packages_linux_gui.nix
+++ common/home-manager/eldios/programs/packages_linux_gui.nix
@@ -1,30 +1,41 @@
 # Packages for Linux-specific graphical user interface tools.
 # This includes applications, theming, and services like gpg-agent.
-{ pkgs, nixpkgs-unstable, lib, ... }: # Added lib for lib.throwIf
+{
+  pkgs,
+  nixpkgs-unstable,
+  lib,
+  ...
+}: # Added lib for lib.throwIf
 let
   unstablePkgs = import nixpkgs-unstable {
     system = pkgs.system; # Use the system from the main pkgs
     config.allowUnfree = true;
   };
 
-  patchelfFixes = pkgs.patchelfUnstable.overrideAttrs (_finalAttrs: _previousAttrs: {
-    src = pkgs.fetchFromGitHub {
-      owner = "Patryk27";
-      repo = "patchelf";
-      rev = "527926dd9d7f1468aa12f56afe6dcc976941fedb";
-      sha256 = "sha256-3I089F2kgGMidR4hntxz5CKzZh5xoiUwUsUwLFUEXqE=";
-    };
-  });
-  pcloud = pkgs.pcloud.overrideAttrs (_finalAttrs:previousAttrs: {
-    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ patchelfFixes ];
-  });
-  mailspring = unstablePkgs.mailspring.overrideAttrs (_finalAttrs: _previousAttrs: {
-    version = "1.15.1";
-    src = unstablePkgs.fetchurl {
-      url = "https://github.com/Foundry376/Mailspring/releases/download/${_finalAttrs.version}/mailspring-${_finalAttrs.version}-amd64.deb";
-      hash = "sha256-+glQaz36mKMtnNeyHH4brZmzYe9SHCtccO6CIJpTH2k=";
-    };
-  });
+  patchelfFixes = pkgs.patchelfUnstable.overrideAttrs (
+    _finalAttrs: _previousAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "Patryk27";
+        repo = "patchelf";
+        rev = "527926dd9d7f1468aa12f56afe6dcc976941fedb";
+        sha256 = "sha256-3I089F2kgGMidR4hntxz5CKzZh5xoiUwUsUwLFUEXqE=";
+      };
+    }
+  );
+  pcloud = pkgs.pcloud.overrideAttrs (
+    _finalAttrs: previousAttrs: {
+      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ patchelfFixes ];
+    }
+  );
+  mailspring = unstablePkgs.mailspring.overrideAttrs (
+    _finalAttrs: _previousAttrs: {
+      version = "1.15.1";
+      src = unstablePkgs.fetchurl {
+        url = "https://github.com/Foundry376/Mailspring/releases/download/${_finalAttrs.version}/mailspring-${_finalAttrs.version}-amd64.deb";
+        hash = "sha256-+glQaz36mKMtnNeyHH4brZmzYe9SHCtccO6CIJpTH2k=";
+      };
+    }
+  );
 
 in
 {
@@ -47,91 +58,94 @@ in
   }; # EOM services
 
   home = {
-    packages = (with pkgs; [
-      # Utils
-      cava
-      cavalier
-      graphviz
-      mission-center
-      playerctl
-      resources
+    packages =
+      (with pkgs; [
+        # Utils
+        cava
+        cavalier
+        graphviz
+        mission-center
+        playerctl
+        resources
 
-      # GUI Applications
-      appimage-run
-      arandr
-      cool-retro-term
-      dia
-      easyeffects
-      filezilla
-      geoclue2
-      gimp-with-plugins
-      gparted
-      gromit-mpx
-      inkscape
-      kitty
-      mosh
-      mpv
-      obs-studio
-      lens
-      paperview
-      pavucontrol
-      pika-backup
-      pulseaudio # to install tools like pactl
-      pcmanfm
-      redshift
-      screenkey
-      scribus
-      signal-desktop
-      slack
-      syncthing
-      telegram-desktop
-      unclutter # unclutter -idle 1 -root -grab -visible
-      vivaldi
-      vivaldi-ffmpeg-codecs
-      vlc
-      whatsapp-for-linux
-      widevine-cdm
-      xclip
-      xdotool
-      zathura
-      zoom-us
+        # GUI Applications
+        appimage-run
+        arandr
+        cool-retro-term
+        dia
+        easyeffects
+        filezilla
+        geoclue2
+        gimp-with-plugins
+        gparted
+        gromit-mpx
+        inkscape
+        kitty
+        mosh
+        mpv
+        obs-studio
+        lens
+        paperview
+        pavucontrol
+        pika-backup
+        pulseaudio # to install tools like pactl
+        pcmanfm
+        redshift
+        screenkey
+        scribus
+        signal-desktop
+        slack
+        syncthing
+        telegram-desktop
+        unclutter # unclutter -idle 1 -root -grab -visible
+        vivaldi
+        vivaldi-ffmpeg-codecs
+        vlc
+        whatsapp-for-linux
+        widevine-cdm
+        xclip
+        xdotool
+        zathura
+        zoom-us
 
-      # Handwriting and Notes
-      krita
-      saber
-      styluslabs-write-bin
-      write_stylus
-      xournalpp
+        # Handwriting and Notes
+        krita
+        saber
+        styluslabs-write-bin
+        write_stylus
+        xournalpp
 
-    ]) ++ (with unstablePkgs; [
-      anytype
-      beeper
-      bitwarden
-      brave
-      cryptomator
-      discord-canary
-      spotify-unwrapped
-      tidal-hifi
-      variety
-      vesktop # discord + some fixes
-      vscode
-      #pdfposter
+      ])
+      ++ (with unstablePkgs; [
+        anytype
+        beeper
+        bitwarden
+        brave
+        cryptomator
+        discord-canary
+        spotify-unwrapped
+        tidal-hifi
+        variety
+        vesktop # discord + some fixes
+        vscode
+        #pdfposter
 
-      # 3D Printing
-      #super-slicer
-      prusa-slicer
-      freecad-wayland
+        # 3D Printing
+        #super-slicer
+        prusa-slicer
+        freecad-wayland
 
-      # 2nd Brain stuff
-      appflowy
-      cameractrls
-      obsidian # Assuming the override is handled or not needed for now
-      rnote
-      streamcontroller
-    ]) ++ [
-      pcloud
-      mailspring
-    ];
+        # 2nd Brain stuff
+        appflowy
+        cameractrls
+        obsidian # Assuming the override is handled or not needed for now
+        rnote
+        #streamcontroller
+      ])
+      ++ [
+        pcloud
+        mailspring
+      ];
   }; # EOM home
 }
 # vim: set ts=2 sw=2 et ai list nu
