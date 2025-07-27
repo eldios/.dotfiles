e64a33bf158fffa63ea15273920723e6a690ce92
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-07-27 19:11:55 +0200

    git: add token to home manager based interactions with github

--- hosts/lele8845ace/home-manager/programs/git.nix
+++ hosts/lele8845ace/home-manager/programs/git.nix
@@ -1,5 +1,26 @@
-{ ... }:
+{ config, ... }:
 {
+  # Nix configuration for user-level commands
+  nix = {
+    extraOptions = ''
+      !include ${config.sops.secrets."tokens/github/nix".path}
+    '';
+
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
 
   programs = {
 
