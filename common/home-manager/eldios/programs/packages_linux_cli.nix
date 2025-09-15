9b34d119c490bee7701e719c4ce2d8eeba96ee9a
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-09-15 13:05:20 +0200

    CLI: added Gemini CLI

--- common/home-manager/eldios/programs/packages_linux_cli.nix
+++ common/home-manager/eldios/programs/packages_linux_cli.nix
@@ -8,26 +8,30 @@ let
 in
 {
   home = {
-    packages = with pkgs; [
-      atop
-      dive
-      docker-slim
-      gcal
-      graph-easy
-      iotop
-      k3s
-      lazydocker
-      ncdu
-      networkmanager
-      ntfs3g
-      p7zip
-      powertop
-      quickemu
-      sshx
-      tty-share
-    ] ++ (with unstablePkgs; [
-      claude-code
-    ]);
+    packages =
+      with pkgs;
+      [
+        atop
+        dive
+        docker-slim
+        gcal
+        graph-easy
+        iotop
+        k3s
+        lazydocker
+        ncdu
+        networkmanager
+        ntfs3g
+        p7zip
+        powertop
+        quickemu
+        sshx
+        tty-share
+      ]
+      ++ (with unstablePkgs; [
+        claude-code
+        gemini-cli
+      ]);
   };
 } # EOF
 # vim: set ts=2 sw=2 et ai list nu
